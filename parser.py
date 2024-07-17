8d7d109df043283f4f2be528fb83e396c6b369c4
Bryan Dalton <bryantdalton19@gmail.com>
2024-07-16 21:00:15 -0400

    parser

--- parser.py
+++ parser.py
@@ -1,23 +1,46 @@
 symbols = {
-  "*": "SOURCE",
-  "═": "HORIZONTAL",
-  "║": "VERTICAL",
-  "╝": "L_UP_LEFT",
-  "╚": "L_UP_RIGHT",
-  "?": "L_DOWN_LEFT",
-  "?": "L_DOWN_RIGHT",
-  "?": "T_UP",
-  "╩": "T_DOWN",
-  "╠": "T_LEFT",
-  "╣": "T_RIGHT",
+    "*": "SOURCE",
+    "═": "HORIZONTAL",
+    "║": "VERTICAL",
+    "╝": "L_UP_LEFT",
+    "╚": "L_UP_RIGHT",
+    "╔": "L_DOWN_LEFT",
+    "╗": "L_DOWN_RIGHT",
+    "╦": "T_UP",
+    "╩": "T_DOWN",
+    "╠": "T_LEFT",
+    "╣": "T_RIGHT",
 }
+
+piece = {
+    "piece": 'SOURCE',
+    "x": 0,
+    "y": 0
+}
+
+
+def get_symbol(str):
+    if str in symbols:
+        return symbols[str]
+    else:
+        return str
+
+
 def read_file(arg):
+    pipes = []
     with open(arg, 'r', encoding='utf-8') as file:
         for line in file:
-           parse_line(line.strip())
+            pipes.append(parse_line(line.strip()))
 
-#get text as arg
+    return pipes
+# get text as arg
+
+
 def parse_line(txt):
-    print(txt.split(' '))
-    # for char in txt:
-    #     print(char.split(' '))
+    parsed_array = txt.split(' ')
+    new_piece = {
+        "piece": get_symbol(parsed_array[0]),
+        'x': parsed_array[1],
+        'y': parsed_array[2]
+    }
+    return new_piece
