e36a8af0ef038cd050c9c8b6ce2405b23dc0db1c
Bryan Dalton <bryantdalton19@gmail.com>
2024-07-30 21:11:56 -0400

    find pipe and return piece

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from parser import read_file 
 import sys
 
+#make prettified maze array
+#line break at max(x)
+#looks like this:
+# * ╣   ╔ ═ A
+#   ╠ ═ ╝    
+#   C   ╚ ═ B
+
 def handle_blank():
     pass
 
+def find_pipe_at_pos(loose_pipes, col, row):
+    for pipe in loose_pipes:
+        if pipe['x'] == col and pipe['y'] == row:
+            return pipe['piece']
+    return " "
 
-
-def create_maze(loose_pipes):
+def create_maze_array(loose_pipes):
     grid_size = determine_grid_size(loose_pipes)
-
+    maze = []
     for col in range(grid_size[0]):
         for row in range(grid_size[1]):
-            print(col, row)
-
-
-
-    # print(loose_pipes)
-    pass
+            #if x = col && y = row: append piece else append " "
+            pipe_result = find_pipe_at_pos(loose_pipes, col, row)
+            # maze.append(f'{col} {row}')
+            maze.append(pipe_result)
+    return maze
 
 def main():
     file_path = sys.argv[1]
     loose_pipes = read_file(file_path)
-    create_maze(loose_pipes)
-
+    maze_array = create_maze_array(loose_pipes)
+    print(maze_array)
 def determine_grid_size(loose_pipes):
     # print(loose_pipes)
     xvals = []
