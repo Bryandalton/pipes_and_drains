de2d629c90f672bd3f095294ad6ad7c552169e89
Bryan Dalton <bryantdalton19@gmail.com>
2024-08-02 16:09:11 -0400

    maze prints correctly

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from parser import read_file 
+from parser import read_file
 import sys
 
-#make prettified maze array
-#line break at max(x)
-#looks like this:
+# make prettified maze array
+# line break at max(x)
+# every 3rd item is a new column
+# looks like this:
 # * ╣   ╔ ═ A
-#   ╠ ═ ╝    
+#   ╠ ═ ╝
 #   C   ╚ ═ B
 
-def handle_blank():
-    pass
 
 def find_pipe_at_pos(loose_pipes, col, row):
     for pipe in loose_pipes:
@@ -17,22 +16,60 @@ def find_pipe_at_pos(loose_pipes, col, row):
             return pipe['piece']
     return " "
 
+
 def create_maze_array(loose_pipes):
     grid_size = determine_grid_size(loose_pipes)
     maze = []
-    for col in range(grid_size[0]):
-        for row in range(grid_size[1]):
-            #if x = col && y = row: append piece else append " "
+    for row in range(grid_size[1]):
+        for col in range(grid_size[0]):
+            # if x = col && y = row: append piece else append " "
             pipe_result = find_pipe_at_pos(loose_pipes, col, row)
             # maze.append(f'{col} {row}')
             maze.append(pipe_result)
     return maze
 
+
+def corrected_maze_matrix(loose_pipes):
+   unaligned_maze = print_maze(loose_pipes)
+   aligned_maze = []
+   for row in unaligned_maze[::-1]:
+       aligned_maze.append(row)
+   return aligned_maze
+       
+    
+   
+       
+
+
+
+#breaks every 3 items into their own arrays
+#would be correct if rotated -90deg
+def print_maze(loose_pipes):
+    maze_matrix = []
+    maze_array = create_maze_array(loose_pipes)
+    start = 0
+    end = len(maze_array)
+    step = 6
+    for i in range(start, end, step):
+        x = i
+        maze_matrix.append(maze_array[x:x+step])
+
+    return maze_matrix
+
+
 def main():
     file_path = sys.argv[1]
     loose_pipes = read_file(file_path)
-    maze_array = create_maze_array(loose_pipes)
-    print(maze_array)
+    # maze_array = create_maze_array(loose_pipes)
+    show_maze = print_maze(loose_pipes)
+    maze = corrected_maze_matrix(loose_pipes)
+    for row in maze:
+        print(row)
+
+
+
+
+
 def determine_grid_size(loose_pipes):
     # print(loose_pipes)
     xvals = []
