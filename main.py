04cc6515bf15ec497a17f5ff56a56c5afead50fe
Bryan Dalton <bryantdalton19@gmail.com>
2024-08-06 20:12:59 -0400

    other-main is now main

--- main.py
+++ main.py
@@ -10,13 +10,31 @@ import sys
 #   C   ╚ ═ B
 
 def main():
+    #read the user input
     file_path = sys.argv[1]
+    
+    # parse it into a bag of pipes
     loose_pipes = read_file(file_path)
-    # maze_array = create_maze_array(loose_pipes)
-    # show_maze = create_maze_matrix(loose_pipes)
-    maze = corrected_maze_matrix(loose_pipes)
-    for row in maze:
-        print(row)
+
+    grid_size = determine_grid_size(loose_pipes)
+
+    # assembled maze from pipes
+    maze_array = create_maze_array(loose_pipes, grid_size)
+
+    # print it
+    print_maze(maze_array, grid_size) 
+
+
+## Make a flat representation of the maze
+def create_maze_array(loose_pipes, grid_size):
+    maze = []
+    for row in range(grid_size[1]):
+        for col in range(grid_size[0]):
+            # if x = col && y = row: append piece else append " "
+            pipe_result = find_pipe_at_pos(loose_pipes, col, row)
+            maze.append(pipe_result)
+    return maze
+    
 
 def find_pipe_at_pos(loose_pipes, col, row):
     for pipe in loose_pipes:
@@ -24,34 +42,23 @@ def find_pipe_at_pos(loose_pipes, col, row):
             return pipe['piece']
     return " "
 
+def determine_grid_size(loose_pipes):
+    # print(loose_pipes)
+    xvals = []
+    yvals= []
+    for i in loose_pipes:
+        xvals.append(i.get('x') + 1)
+        yvals.append(i.get('y') + 1)
 
-def create_maze_array(loose_pipes):
-    grid_size = determine_grid_size(loose_pipes)
-    maze = []
-    for row in range(grid_size[1]):
-        for col in range(grid_size[0]):
-            # if x = col && y = row: append piece else append " "
-            pipe_result = find_pipe_at_pos(loose_pipes, col, row)
-            # maze.append(f'{col} {row}')
-            maze.append(pipe_result)
-    return maze
-
-
-def corrected_maze_matrix(loose_pipes):
-   unaligned_maze = create_maze_matrix(loose_pipes)
-#    print(unaligned_maze)
-   aligned_maze = []
-   for row in unaligned_maze[::-1]:
-       aligned_maze.append(row)
-   return aligned_maze
-       
-
-def create_maze_matrix(loose_pipes):
+    return (max(xvals), max(yvals))
+# take the flat maze, and turn it into a 2d matrix
+# but its backwards!
+def create_maze_matrix(maze_array, grid_size):
     maze_matrix = []
-    maze_array = create_maze_array(loose_pipes)
     start = 0
     end = len(maze_array)
-    step = 6
+    # print(grid_size[0])
+    step = grid_size[0]
     for i in range(start, end, step):
         x = i
         maze_matrix.append(maze_array[x:x+step])
@@ -59,18 +66,21 @@ def create_maze_matrix(loose_pipes):
     return maze_matrix
 
 
-def determine_grid_size(loose_pipes):
-    # print(loose_pipes)
-    xvals = []
-    yvals= []
-    for i in loose_pipes:
-        xvals.append(i.get('x') + 1)
-        yvals.append(i.get ('y') + 1)
+def print_maze(maze_array, grid_size):
+    # convert it into a matrix so we can print it
+    maze_matrix = create_maze_matrix(maze_array, grid_size)
+    
+    aligned_mazed = correct_maze_matrix(maze_matrix)
+    for row in aligned_mazed:
+        print(' '.join(row))
 
-    return (max(xvals), max(yvals))
-
-
+# reverse the rows of the maze for better printing
+def correct_maze_matrix(unaligned_maze):
+   aligned_maze = []
+   for row in unaligned_maze[::-1]:
+       aligned_maze.append(row)
+   return aligned_maze
+       
 
 if __name__ == "__main__":
     main()
-
